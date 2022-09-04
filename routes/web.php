2217a301b929a9fea78731c0af891b7bd7235aa0
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2022-09-04 12:12:06 +0600

    Basic Setup done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('judge_info', function () {
+    $response = Http::get('http://localhost:2358/system_info');
+    return $response->json();
+});
+
+Route::get('languages', function () {
+    $response = Http::get('http://bd63-118-179-107-241.ngrok.io/languages',['verify' => false]);
+    return $response->json();
 });
